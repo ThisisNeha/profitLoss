92dae33ca5d0bb01a65064bb8f7710cf74d3b0d8
NehaBarak <nehabarak45@gmail.com>
2021-09-21 11:55:37 +0530

    update : links updated

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ function calculateProfitLoss(initial, quantity, current) {
 if (initial > current){
   var loss = (initial - current)*quantity ; 
   var lossPercent = (loss/initial)*100;
-  outputDiv.style.color="red";
+  outputDiv.style.color="orangered";
   showOutput(`Oops! your loss is ${loss} and loss percent is ${lossPercent.toFixed(2)} %🙄.`); 
 }
 else if(initial<current){
   var profit = (current-initial)*quantity ; 
   var profitPercent = (profit/initial)*100;
   showOutput(`Yaay! your profit is ${profit} and profit percent is ${profitPercent.toFixed(2)} % 😄.`); 
-  outputDiv.style.color="navy";
+  outputDiv.style.color="blue";
 }
 else{
   showOutput(`No pain no gain and no gain pain 😬!`);
